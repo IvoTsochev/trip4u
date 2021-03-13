d77de1e25b8153d10ae15533fde13949fba73acd
IvoTsochev <ivo.tsochev@live.com>
2021-03-13 16:31:00 +0200

    installing styled-components and creating global styling file

---
+++ src/Pages/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// Styling
+import './Welcome.scss'
+import { Button } from '../../globalStyling';
+
+const Welcome = () => {
+    return (
+        <div className='welcome'>
+            <h1>Trip4u</h1>
+            <Button />
+        </div>
+    )
+}
+
+export default Welcome
---
+++ src/globalStyling.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+    width: 200px;
+    height: 40px;
+
+`
+
+export {
+    Button,
+}
