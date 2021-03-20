f7c6ba743e159af38d3abd84fb66a3c928759a26
IvoTsochev <ivo.tsochev@live.com>
2021-03-20 17:49:51 +0200

    Travel you component code mockup

--- src/Pages/TravelYou/TravelYou.js
+++ src/Pages/TravelYou/TravelYou.js
@@ -9,6 +9,27 @@ const TravelYou = () => {
 
     return (
         <div className='travelyou'>
+            <div className="travelyou__dashboard">
+                <div className="travelyou__input">
+                    <p>I'm offering a ride!</p>
+                    <div className="travelyou--from">
+                        <p>From:</p>
+                        {/* input country */}
+                        {/* input city */}
+                    </div>
+                    <div className="travelyou--to">
+                        <p>To:</p>
+                        {/* input country */}
+                        {/* input city */}
+                    </div>
+                    {/* input number */}
+                    {/* input seats */}
+                    {/* input time departure */}
+                </div>
+                <div className="travelyou__map">
+                    {/* google maps */}
+                </div>
+            </div>
         </div>
     )
 }
