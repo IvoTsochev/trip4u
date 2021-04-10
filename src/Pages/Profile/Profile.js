31371ae452f351b3f0240984d78219ab12567537
IvoTsochev <ivo.tsochev@live.com>
2021-04-10 19:08:33 +0300

    profile page update

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,10 +1,29 @@
+// Utils
+import { useStateValue } from '../../StateProvider';
+import defImg from '../../media/defaultProfileImg.png';
 // Styling
-import './Profile.scss'
+import './Profile.scss';
 
 const Profile = () => {
+
+    const [{ user }, dispatch] = useStateValue();
+
+    let photoUrl = '';
+
+    if (user.photoURL) {
+        photoUrl = user.photoURL
+    } else {
+        photoUrl = defImg;
+    }
+
+
     return (
-        <div>
-            <p>THIS IS THE PROFILE PAGE</p>
+        <div className='profile'>
+            <h2>Dashboard</h2>
+            <p>Username: {user.displayName}</p>
+            <p>Email address: {user.email}</p>
+            <p>Last login: {user.metadata.lastSignInTime}</p>
+            <img src={photoUrl} alt="default image" width='150' />
         </div>
     )
 }
